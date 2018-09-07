1eed6025960dffce14776deb9d6bc3328f7a0072
Kiley Delaney <kdelaney@solstice.com>
2018-09-07 13:54:52 -0500

    First commit

---
+++ microservices-ex/src/main/java/com/kileydelaney/microservicesex/MicroservicesExApplication.java
@@ -0,0 +1,12 @@
+package com.kileydelaney.microservicesex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesExApplication.class, args);
+    }
+}
---
+++ microservices-ex/src/test/java/com/kileydelaney/microservicesex/MicroservicesExApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kileydelaney.microservicesex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesExApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
